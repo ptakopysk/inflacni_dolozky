5fd70f39536c10fe3dd392d42adfc7e7b56032b6
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-04-30 14:04:16 +0200

    fixed

--- pokus.py
+++ pokus.py
@@ -14,17 +14,26 @@ logging.basicConfig(
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 model_opts = [
-        #"lchaloupsky/czech-gpt2-oscar",
-        #"BUT-FIT/Czech-GPT-2-XL-133k",
+        "lchaloupsky/czech-gpt2-oscar",
+        "BUT-FIT/Czech-GPT-2-XL-133k",
         "BUT-FIT/CSTinyLlama-1.2B",
         "simecek/cswikimistral_0.1"
         ]
+max_lengths = [
+        1024,
+        1024,
+        2048,
+        32768
+        ]
+output_len = 512
 prompt_opts = ["Velikost inflační doložky za rok 2017?", "Co se v předchozím textu píše o inflační doložce?", "Vypiš z předchozího textu informace týkající se inflační doložky."]
 
 for model_index in range(len(model_opts)):
     model_name = model_opts[model_index]
     logging.info(f"Load model {model_name}")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model_max_len = max_lengths[model_index]
+    tokenizer.model_max_length = model_max_len
     model = AutoModelForCausalLM.from_pretrained(model_name)
 
     logging.info("Process files")
@@ -35,11 +44,14 @@ for model_index in range(len(model_opts)):
                 prompt = prompt_opts[prompt_index]
                 full_prompt = text + "\n\n" + prompt
                 logging.info(full_prompt)
-                tokenized_prompt = tokenizer.encode(full_prompt, return_tensors='pt')
+                tokenized_prompt = tokenizer.encode(full_prompt,
+                        return_tensors='pt',
+                        max_length=model_max_len-output_len,
+                        truncation=True)
                 logging.info(tokenized_prompt)
                 out = model.generate(
                         tokenized_prompt,
-                        max_new_tokens=512,
+                        max_new_tokens=output_len,
                         pad_token_id= tokenizer.pad_token_id,
                         eos_token_id = tokenizer.eos_token_id)
                 decoded = tokenizer.decode(out[0], skip_special_tokens=True)
--- pokus2.py
+++ pokus2.py
@@ -19,20 +19,34 @@ model_opts = [
         "BUT-FIT/CSTinyLlama-1.2B",
         "simecek/cswikimistral_0.1"
         ]
+max_lengths = [
+        1024,
+        1024,
+        2048,
+        32768
+        ]
+output_len = 512
+
 for model_index in range(len(model_opts)):
     model_name = model_opts[model_index]
     logging.info(f"Load model {model_name}")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model_max_len = max_lengths[model_index]
+    tokenizer.model_max_length = model_max_len
     model = AutoModelForCausalLM.from_pretrained(model_name)
 
-    full_prompt = "Jak se Napoleon zove?"
+
+    full_prompt = 1000 * "Jak se Napoleon zove? "
     logging.info(full_prompt)
-    tokenized_prompt = tokenizer.encode(full_prompt, return_tensors='pt')
+    tokenized_prompt = tokenizer.encode(full_prompt,
+            return_tensors='pt',
+            max_length=model_max_len-output_len,
+            truncation=True)
     logging.info(tokenized_prompt)
     
     out = model.generate(
             tokenized_prompt,
-            max_new_tokens=512,
+            max_new_tokens=output_len,
             pad_token_id= tokenizer.pad_token_id,
             eos_token_id = tokenizer.eos_token_id)
     decoded = tokenizer.decode(out[0], skip_special_tokens=True)
